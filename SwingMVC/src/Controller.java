e91d456a7ef41c4a4329e959a2192d5e0d43c756
Khushi Patel <khuship2708@gmail.com>
2025-10-06 18:23:38 -0400

    Update Controller.java

--- SwingMVC/src/Controller.java
+++ SwingMVC/src/Controller.java
@@ -1,43 +1,32 @@
-import javax.swing.JOptionPane;
- 
+//Controller.java: It connects model, view and scanner.
+//When "scan" is pressed, gets a random UPC from scanner, adds product to CaaRegister and updates View
+
 public class Controller {
-	 private Model model;
-	 private View view;
-	 
-	 public Controller(Model m, View v) {
-	  model = m;
-	  view = v;
-	  initView();
-	 }
-	 
-	 public void initView() {
-	  view.getFirstnameTextfield().setText(model.getFirstname());
-	  view.getLastnameTextfield().setText(model.getLastname());
-	 }
-	 
-	 public void initController() {
-	  view.getFirstnameSaveButton().addActionListener(e -> saveFirstname());
-	  view.getLastnameSaveButton().addActionListener(e -> saveLastname());
-	  view.getHello().addActionListener(e -> sayHello());
-	  view.getBye().addActionListener(e -> sayBye());
-	 }
-	 
-	 private void saveFirstname() {
-	  model.setFirstname(view.getFirstnameTextfield().getText());
-	  JOptionPane.showMessageDialog(null, "Firstname saved : " + model.getFirstname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void saveLastname() {
-	  model.setLastname(view.getLastnameTextfield().getText());
-	  JOptionPane.showMessageDialog(null, "Lastname saved : " + model.getLastname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void sayHello() {
-	  JOptionPane.showMessageDialog(null, "Hello " + model.getFirstname() + " " + model.getLastname(), "Info", JOptionPane.INFORMATION_MESSAGE);
-	 }
-	 
-	 private void sayBye() {
-	  System.exit(0);
-	 }
-	 
+    private CashRegister model;
+    private View view;
+    private Scanner scanner;
+
+    public Controller(CashRegister m, View v, Scanner s) {
+        this.model = m;
+        this.view = v;
+        this.scanner = s;
+        initController();
+    }
+
+    // Scan button listener
+    public void initController() {
+        scanner.getScanButton().addActionListener(e -> scanProduct());
+    }
+
+    // Handle scanning a product
+    private void scanProduct() {
+        String upc = scanner.generateUPC(); // get random UPC
+        model.addProduct(upc);               // add to model
+
+        Product p = model.getProduct(upc);
+        if (p != null) {
+            view.addItem(p.toString());         // show in list
+            view.updateSubtotal(model.getSubtotal()); // update subtotal
+        }
+    }
 }
